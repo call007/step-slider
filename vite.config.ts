a181824c62ed7754284e380b9c6a3d40c6864673
Ed Shevtsov <call007.ed@gmail.com>
2025-03-07 19:30:12 +0100

    fix: deploy to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/step-slider/",
+  base: process.env.VITE_BASE_URL,
   build: {
     emptyOutDir: true,
   },
   plugins: [
     react({
-      jsxImportSource: '@emotion/react',
+      jsxImportSource: "@emotion/react",
       babel: {
-        plugins: ['@emotion/babel-plugin'],
+        plugins: ["@emotion/babel-plugin"],
       },
     }),
   ],
